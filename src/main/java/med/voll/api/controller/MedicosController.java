26b1771e82c8dec59e063c8cc72c07eed2cfdb1f
Ze Nobody <gabriel296296@gmail.com>
2023-06-07 20:32:10 -0300

    att

---
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -0,0 +1,19 @@
+package med.voll.api.controller;
+
+import med.voll.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("medicos")
+@RestController
+public class MedicosController {
+
+    @PostMapping
+    public void cadastrar (@RequestBody DadosCadastroMedico dados) {
+
+        System.out.println(dados);
+    }
+}
