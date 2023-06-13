f819add868db91eae6673d1430550f11d73266cf
Ze Nobody <gabriel296296@gmail.com>
2023-06-12 21:21:16 -0300

    cadastro no banco de dados

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medicos.DadosCadastroMedico;
+import med.voll.api.medicos.Medico;
+import med.voll.api.medicos.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MedicosController {
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
     public void cadastrar (@RequestBody DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
 
-        System.out.println(dados);
     }
 }
