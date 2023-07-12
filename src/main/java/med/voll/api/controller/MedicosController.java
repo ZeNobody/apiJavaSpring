59be3974abcdf68652218cbd76b29d2ef86778be
ZeNobody <gabriel296296@example.com>
2023-07-12 00:11:53 -0300

    Atualização e Exclusão Médicos

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,9 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medicos.DadosCadastroMedico;
-import med.voll.api.medicos.DadosListagemMedico;
-import med.voll.api.medicos.Medico;
-import med.voll.api.medicos.MedicoRepository;
+import med.voll.api.medicos.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -32,6 +29,21 @@ public class MedicosController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAttMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.attInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir (@PathVariable Long id) {
+        // repository.getReferenceById(id);     Deleta todos os dados do banco
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
--- src/main/java/med/voll/api/medicos/Medico.java
+++ src/main/java/med/voll/api/medicos/Medico.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
+import org.springframework.http.ResponseEntity;
 
 @Table(name="medicos")
 @Entity(name="Medico")
@@ -26,12 +27,31 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome=dados.nome();
         this.email=dados.email();
         this.telefone=dados.telefone();
         this.crm=dados.crm();
         this.especialidade=dados.especialidade();
         this.endereco=new Endereco(dados.endereco());
+    }
+
+    public void attInformacoes(DadosAttMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null) {
+            this.endereco.attInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
